8692ae2520acabbb42cc9700703e61462cd65f5d
Eric Dunn <ericlewisdunn@gmail.com>
2023-04-13 12:01:08 -0400

    start of react testing

--- __tests__/location.test.js
+++ __tests__/location.test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 const server = require('../server/server');
 
-describe('POST api/location', () => {
+xdescribe('POST api/location', () => {
     let authToken = 'Bearer OmgJFQk8VBZfv0zC6St4qG5X1zfA694YbdWZlvFmF09GAU9vSgR56VG0g7FpjJf2XHGqO6kfeypyDcrKMkLMo_CzP4ED7v9XXodQazsa_YYnXIpy-kRkjs9z-DE0ZHYx'
     it('POST /api/users should return 200 with authentication', async () => {
         const res = await request(server).post("/api/location/tokyo").send({
--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 const request = require("supertest");
 const server = require('../server/server');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
 
 require("dotenv").config();
 
@@ -17,7 +15,7 @@ beforeEach(async () => {
   });
 
   //test login post request
-xdescribe("POST /api/user/login", () => {
+describe("POST /api/user/login", () => {
     it("should return the user object with hashed password", async () => {
         const res = await request(server).post("/api/user/login").send({
             username: 'garyb',
@@ -45,14 +43,14 @@ xdescribe("POST /api/user/login", () => {
     })
 });
 
-xdescribe("POST /api/user/signup", () => {
+describe("POST /api/user/signup", () => {
     it("should return user object with hashed password", async () => {
         const res = await request(server).post("/api/user/signup").send({
-            username:'joh',
+            username:'pollyjones',
             password:'hi'
         });
         expect(res.status).toBe(200);
-        expect(res.body.username).toBe('joh');
+        expect(res.body.username).toBe('pollyjones');
         expect(res.body.password).not.toBe('hi');
 
     });
